6a58024903c80344c1bd6c8772a02fa46d1b4f26
Katla Mrudul <Bittu96@users.noreply.github.com>
2021-04-19 18:54:12 +0530

    Add files via upload

--- Assignment/rotate_matrix_anti_clockwise.py
+++ Assignment/rotate_matrix_anti_clockwise.py
@@ -1,9 +1,7 @@
 sample_matrix_1 = [
-    [1,  2,  3,  4,   5],
-    [6,  7,  8,  9,  10],
-    [11, 12, 13, 14, 15],
-    [16, 17, 18, 19, 20],
-    [21, 22, 23, 24, 25]
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
 ]
 
 sample_matrix_2 = [
@@ -11,6 +9,14 @@ sample_matrix_2 = [
     [5,  6,  7, 8],
     [9, 10, 11, 12],
     [13, 14, 15, 16]
+]
+
+sample_matrix_3 = [
+    [1,  2,  3,  4,   5],
+    [6,  7,  8,  9,  10],
+    [11, 12, 13, 14, 15],
+    [16, 17, 18, 19, 20],
+    [21, 22, 23, 24, 25]
 ]
 
 # one rotation = 90 degree rotation anti clock-wise
@@ -54,14 +60,35 @@ def matrix_rotation(matrix, rotations):
     return matrix
 
 
-# choose sample matrix
-matrix = sample_matrix_2
-
-# before rotation
-print(*matrix, sep="\n")
+rotated_matrix = matrix_rotation(sample_matrix_1, 1)
+print(*rotated_matrix, sep="\n")
 
 print()
 
-# after rotation
-rotated_matrix = matrix_rotation(matrix, 1)
+rotated_matrix = matrix_rotation(sample_matrix_2, 1)
 print(*rotated_matrix, sep="\n")
+
+print()
+
+rotated_matrix = matrix_rotation(sample_matrix_3, 1)
+print(*rotated_matrix, sep="\n")
+
+'''
+solution: 
+
+[3, 6, 9]
+[2, 5, 8]
+[1, 4, 7]
+
+[4, 8, 12, 16]
+[3, 7, 11, 15]
+[2, 6, 10, 14]
+[1, 5, 9, 13]
+
+[5, 10, 15, 20, 25]
+[4, 9, 14, 19, 24]
+[3, 8, 13, 18, 23]
+[2, 7, 12, 17, 22]
+[1, 6, 11, 16, 21]
+
+'''
--- Assignment/sum_of_bit_differences.py
+++ Assignment/sum_of_bit_differences.py
@@ -1,22 +1,36 @@
-arr = [1, 2]
-sum_of_bit_differences = 0
+sample_array_1 = [1, 2]
+sample_array_2 = [1,3,5]
 
-# going through each and every pair
+def compute_bit_difference(array):
+	sum_of_bit_differences = 0
 
-for i in arr:
-    for j in arr:
+	# going through each and every pair
 
-        # finding bit differences which is bacically XOR
-        # (as XOR returns 1 if not same)
-        # here it returns string of 0s or 1s as comparisions
-        # and formatting as a binary
-        bit_difference = f"{i^j:b}"
+	for i in array:
+	    for j in array:
 
-        # counting for ones which is
-        # basically the differences in bits
-        count_of_ones = bit_difference.count("1")
+	        # finding bit differences which is bacically XOR
+	        # (as XOR returns 1 if not same)
+	        # here it returns string of 0s or 1s as comparisions
+	        # and formatting as a binary
+	        bit_difference = f"{i^j:b}"
 
-        # adding the differences
-        sum_of_bit_differences += count_of_ones
+	        # counting for ones which is
+	        # basically the differences in bits
+	        count_of_ones = bit_difference.count("1")
 
-print(sum_of_bit_differences)
+	        # adding the differences
+	        sum_of_bit_differences += count_of_ones
+
+	print(sum_of_bit_differences)
+
+compute_bit_difference(sample_array_1)
+compute_bit_difference(sample_array_2)
+
+'''
+solution
+
+4
+8
+
+'''
